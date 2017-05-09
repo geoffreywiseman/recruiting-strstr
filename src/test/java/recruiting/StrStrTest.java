425f09bcc46d5f70a9f412b19785ddede202a8bb
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2017-05-09 15:42:09 -0400

    Formatting.

--- src/test/java/recruiting/StrStrTest.java
+++ src/test/java/recruiting/StrStrTest.java
@@ -6,48 +6,48 @@ import org.junit.Test;
 public abstract class StrStrTest {
 
 	public abstract int strstr( char[] pattern, char[] string );
-	
+
 	public int strstr( String pattern, String string ) {
 		return strstr( pattern.toCharArray(), string.toCharArray() );
 	}
-	
+
 	@Test
 	public void testSimpleFind() {
 		Assert.assertEquals( 0, strstr( "pat", "pathology" ) );
 	}
-	
+
 	@Test
 	public void testNotFound() {
 		Assert.assertEquals( -1, strstr( "pat", "dementia" ) );
 	}
-	
+
 	@Test
 	public void testNotFirstFirstCharacterMatch() {
 		Assert.assertEquals( 4, strstr( "pat", "pitapat" ) );
 	}
-	
+
 	@Test
 	public void testPatternLongerThanString() {
 		Assert.assertEquals( -1, strstr( "pattern", "str" ) );
 	}
-	
+
 	@Test
 	public void testStringEndsBeforePattern() {
 		Assert.assertEquals( -1, strstr( "pat", "oompa" ) );
 	}
-	
+
 	@Test
 	public void testMultipleMatches() {
 		Assert.assertEquals( 4, strstr( "pat", "papapatpat" ) );
 	}
 
-        @Test
+	@Test
 	public void testDontSkipOverMismatchedPattern() {
-                Assert.assertEquals( 2, strstr( "lola", "lololala" ) );
-        }
+		Assert.assertEquals( 2, strstr( "lola", "lololala" ) );
+	}
 
-        @Test
-        public void testFindExact() {
+	@Test
+	public void testFindExact() {
 		Assert.assertEquals( 0, strstr( "bird", "bird" ) );
 	}
 
@@ -61,5 +61,4 @@ public abstract class StrStrTest {
 		Assert.assertEquals( -1, strstr( "mistake", "mistaki" ) );
 	}
 
-	
 }
